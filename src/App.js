1a45b5ad3547382a2cf3304a272dba719a731451
quedacoder <quedacoder@gmail.com>
2021-08-03 23:46:21 -0500

    Added Accordion functionality

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
 import React from 'react';
+import Accordion from './components/Accordion';
+
+const items = [
+    {
+        title: 'What is react?',
+        content: 'React is a front end javascript framework'
+    },
+    {
+        title: 'Why use react?',
+        content: 'React is a favorite JS library among engineers'
+    },
+    {
+        title: 'How do you use react?',
+        content: 'You use React by creating components'
+    }
+];
 
 export default () => {
-    return <h1>Widgets App</h1>;
+
+    return (
+        <div>
+            <br />
+            <Accordion items={items} />
+        </div>
+        
+    );
 };
